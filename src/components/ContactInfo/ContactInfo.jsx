2eca7088962920699d6562c05a91286f064bc5c4
laurisfree <freelau00@gmail.com>
2022-12-01 12:01:48 -0800

    styling changes

--- src/components/ContactInfo/ContactInfo.jsx
+++ src/components/ContactInfo/ContactInfo.jsx
@@ -7,7 +7,7 @@ export default function ContactInfo() {
             <img src="" alt="logo" className=''/>
         </div> */}
             <div className='c-info'>
-                <form action="" className='c-info__form'>                
+                {/* <form action="" className='c-info__form'>                 */}
                     <h2 className='c-info__subheader'>How can our team contact you?</h2>
 
                     <p className='c-info__paragraph'>Before we can get your free month of bookkeeping started, we’ll need a few more details. Let us know the best phone number to reach you, and we’ll get in touch ASAP to get started.</p>
@@ -52,7 +52,7 @@ export default function ContactInfo() {
                                 <option value="" className='c-info__form-input-time-combo'>Hawaii-Aleutian (UTC-10)</option>
                             </select>
                     </label>
-                </form>
+                {/* </form> */}
 		    </div>
 		</>
 	);
