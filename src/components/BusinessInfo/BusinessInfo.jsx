f790b9278516867ea1853d4d8bc17b225e81da89
laurisfree <freelau00@gmail.com>
2022-11-30 20:00:35 -0800

    form structure

---
+++ src/components/BusinessInfo/BusinessInfo.jsx
@@ -0,0 +1,52 @@
+import './BusinessInfo.scss';
+
+export default function BusinessInfo() {
+	return (
+		<>
+        <div>
+            <img src="" alt="logo" className=''/>
+        </div>
+        <div className='b-info'>
+            <form action="" className='b-info__form'>
+                <h2 className='b-info__form-header'>Tell us about your business</h2>
+
+                <label htmlFor="" className='b-info__form-label'>1. Company Size</label>
+                <input type="text" />
+
+                <label htmlFor="" className='b-info__form-label'>2. Monthly Revenue</label>
+                <input type="text" className='b-info__form-input'/>
+
+                <label htmlFor="" className='b-info__form-label'>3. Monthly Revenue</label>
+                <input type="text" className='b-info__form-input'/>
+
+                <br />
+
+                <h2 className='b-info-header'>Contact</h2>
+
+                <label htmlFor="" className='b-info__form-label'>1. Company Name</label>
+                <input type="text" className='b-info__form-input'/>
+
+                <label htmlFor="" className='b-info__form-label'>2. Phone Number</label>
+                <input type="text" className='b-info__form-input' />
+
+                <label htmlFor="" className='b-info__form-label'>3. Country</label>
+                <input type="text" className='b-info__form-input'/>
+
+                <label htmlFor="" className='b-info__form-label'>3. Time zone</label>
+                <input type="text" className='b-info__form-input' />
+            </form>
+            <div className='b-info__side-wrapper'>
+                <div className='b-info__side-wrapper-up'>
+                    <img src="" alt="" />
+                    <h2></h2>
+                    <p></p>
+                </div>
+                <div className='b-info__side-wrapper-down'>
+                    <img src="" alt="" />
+                    <p></p>
+                </div>
+            </div>
+		</div>
+		</>
+	);
+}
