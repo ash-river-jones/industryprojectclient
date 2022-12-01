955d8d55bac30cc4afb9bf186d41e46810e84e60
laurisfree <freelau00@gmail.com>
2022-11-30 20:24:09 -0800

    form structure

--- src/components/BusinessInfo/BusinessInfo.jsx
+++ src/components/BusinessInfo/BusinessInfo.jsx
@@ -29,11 +29,17 @@ export default function BusinessInfo() {
                 <label htmlFor="" className='b-info__form-label'>2. Phone Number</label>
                 <input type="text" className='b-info__form-input' />
 
+                <div className='b-info-wrper-tags'>
+
                 <label htmlFor="" className='b-info__form-label'>3. Country</label>
                 <input type="text" className='b-info__form-input'/>
 
                 <label htmlFor="" className='b-info__form-label'>3. Time zone</label>
                 <input type="text" className='b-info__form-input' />
+
+                </div>
+
+                <button></button>
             </form>
             <div className='b-info__side-wrapper'>
                 <div className='b-info__side-wrapper-up'>
