2361e02052d6d92a5345efe1f1451b91a7810011
Cale <cale.tian@icloud.com>
2022-12-01 12:29:48 -0800

    making changes

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -15,7 +15,6 @@ import { useEffect, useState } from "react";
 const { v4: uuid } = require("uuid");
 
 export default function Form() {
-<<<<<<< HEAD
 	const navigate = useNavigate();
 	const [companySize, setCompanySize] = useState('');
 	const [monthyRevenue, setMonthyRevenue] = useState('');
@@ -28,21 +27,6 @@ export default function Form() {
 	const [lastName, setLastName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
-=======
-  const navigate = useNavigate();
-
-  const [companySize, setCompanySize] = useState("");
-  const [monthyRevenue, setMonthyRevenue] = useState("");
-  const [lastTaxReturn, setLastTaxReturn] = useState("");
-  const [companyName, setCompanyName] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [country, setCountry] = useState("");
-  const [timeZone, setTimeZone] = useState("");
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
->>>>>>> dev
 
   const [companySizeError, setCompanySizeError] = useState(null);
   const [monthyRevenueError, setMonthyRevenueError] = useState(null);
@@ -212,7 +196,6 @@ export default function Form() {
     setTimeZone(timeZone);
   };
 
-<<<<<<< HEAD
 	console.log(formData)
 	return (
 		<>
@@ -232,7 +215,7 @@ export default function Form() {
 												<label className="company__info--input__label" htmlFor="size-1">1</label>
 												<input type="radio" id="size-2" className="company__info--input__radio" name="size" value="2-5" onChange={(e) => {setCompanySize(e.target.value)}} checked={formData.company_size === "2-5" ? true : false}></input>
 												<label className="company__info--input__label" htmlFor="size-2">2-5</label>
-												<input type="radio" id="size-3" className="company__info--input__radio" name="size" value="6-10" onChange={(e) => {setCompanySize(e.target.value)}} checked={formData.company_size === "6-11" ? true : false}></input>
+												<input type="radio" id="size-3" className="company__info--input__radio" name="size" value="6-10" onChange={(e) => {setCompanySize(e.target.value)}} checked={formData.company_size === "6-10" ? true : false}></input>
 												<label className="company__info--input__label" htmlFor="size-3">6-10</label>
 												<input type="radio" id="size-4" className="company__info--input__radio" name="size" value="11-25" onChange={(e) => {setCompanySize(e.target.value)}} checked={formData.company_size === "11-25" ? true : false}></input>
 												<label className="company__info--input__label" htmlFor="size-4">11-25</label>
@@ -270,275 +253,6 @@ export default function Form() {
 								</section>
 								<section className='contact-info'>
 									<div className='c-info'>
-=======
-
-  return (
-    <>
-      <div className="form-wrapper">
-        <PageHeader />
-        <main className="form-main">
-          <form onSubmit={handelFormSubmit}>
-            {!formPartOneDone && (
-              <section>
-                <section className="company-size">
-                  <div className="company__info">
-                    <h1>Tell Us About Your Business</h1>
-                    <div className="company__info--input">
-                      <label>1. Company Size</label>
-                      <div className="company__info--input__list">
-                        <input
-                          type="radio"
-                          id="size-1"
-                          className="company__info--input__radio"
-                          name="size"
-                          value="1"
-                          onChange={(e) => {
-                            setCompanySize(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="size-1"
-                        >
-                          1
-                        </label>
-                        <input
-                          type="radio"
-                          id="size-2"
-                          className="company__info--input__radio"
-                          name="size"
-                          value="2-5"
-                          onChange={(e) => {
-                            setCompanySize(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="size-2"
-                        >
-                          2-5
-                        </label>
-                        <input
-                          type="radio"
-                          id="size-3"
-                          className="company__info--input__radio"
-                          name="size"
-                          value="6-10"
-                          onChange={(e) => {
-                            setCompanySize(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="size-3"
-                        >
-                          6-10
-                        </label>
-                        <input
-                          type="radio"
-                          id="size-4"
-                          className="company__info--input__radio"
-                          name="size"
-                          value="11-25"
-                          onChange={(e) => {
-                            setCompanySize(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="size-4"
-                        >
-                          11-25
-                        </label>
-                        <input
-                          type="radio"
-                          id="size-5"
-                          className="company__info--input__radio"
-                          name="size"
-                          value="25+"
-                          onChange={(e) => {
-                            setCompanySize(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="size-5"
-                        >
-                          25+
-                        </label>
-                        <input
-                          type="radio"
-                          id="size-6"
-                          className="company__info--input__radio"
-                          name="size"
-                          value="not sure"
-                          onChange={(e) => {
-                            setCompanySize(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="size-6"
-                        >
-                          I'm not sure
-                        </label>
-                      </div>
-                      <label>2. Monthly Revenue</label>
-                      <div className="company__info--input__list">
-                        <input
-                          type="radio"
-                          id="rev-1"
-                          className="company__info--input__radio"
-                          name="revenue"
-                          value="0"
-                          onChange={(e) => {
-                            setMonthyRevenue(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="rev-1"
-                        >
-                          $0 (Pre-revenue)
-                        </label>
-                        <input
-                          type="radio"
-                          id="rev-2"
-                          className="company__info--input__radio"
-                          name="revenue"
-                          value="$1-$1,000"
-                          onChange={(e) => {
-                            setMonthyRevenue(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="rev-2"
-                        >
-                          $1-$1,000
-                        </label>
-                        <input
-                          type="radio"
-                          id="rev-3"
-                          className="company__info--input__radio"
-                          name="revenue"
-                          value="$1,000-$10,000"
-                          onChange={(e) => {
-                            setMonthyRevenue(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="rev-3"
-                        >
-                          $1,000-$10,000
-                        </label>
-                        <input
-                          type="radio"
-                          id="rev-4"
-                          className="company__info--input__radio"
-                          name="revenue"
-                          value="$10,000-$100,000"
-                          onChange={(e) => {
-                            setMonthyRevenue(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="rev-4"
-                        >
-                          $10,000-$100,000
-                        </label>
-                        <input
-                          type="radio"
-                          id="rev-5"
-                          className="company__info--input__radio"
-                          name="revenue"
-                          value="more than $100,000"
-                          onChange={(e) => {
-                            setMonthyRevenue(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="rev-5"
-                        >
-                          more than $100,000
-                        </label>
-                      </div>
-                      <label>3. Last Tax Return</label>
-                      <div className="company__info--input__list">
-                        <input
-                          type="radio"
-                          id="year-1"
-                          className="company__info--input__radio"
-                          name="year"
-                          value="2021"
-                          onChange={(e) => {
-                            setLastTaxReturn(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="year-1"
-                        >
-                          Tax Year 2021
-                        </label>
-                        <input
-                          type="radio"
-                          id="year-2"
-                          className="company__info--input__radio"
-                          name="year"
-                          value="2020"
-                          onChange={(e) => {
-                            setLastTaxReturn(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="year-2"
-                        >
-                          Tax Year 2020
-                        </label>
-                        <input
-                          type="radio"
-                          id="year-3"
-                          className="company__info--input__radio"
-                          name="year"
-                          value="2019"
-                          onChange={(e) => {
-                            setLastTaxReturn(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="year-3"
-                        >
-                          Tax Year 2019 or prior
-                        </label>
-                        <input
-                          type="radio"
-                          id="year-4"
-                          className="company__info--input__radio"
-                          name="year"
-                          value="n/a"
-                          onChange={(e) => {
-                            setLastTaxReturn(e.target.value);
-                          }}
-                        ></input>
-                        <label
-                          className="company__info--input__label"
-                          htmlFor="year-4"
-                        >
-                          My business is new, I'm not sure.
-                        </label>
-                      </div>
-                    </div>
-                  </div>
-                </section>
-                <section className="contact-info">
-                  {/* <div className='c-info'>
->>>>>>> dev
 											<h2 className='c-info__subheader'>Contact</h2>
 											<label htmlFor="companyname" className='c-info__form-label'>1. Company Name
 												<input type="text" className='c-info__form-input-name' value={companyName} onChange={(e) => {setCompanyName(e.target.value)}}/>
@@ -556,7 +270,7 @@ export default function Form() {
 
 											</div>
 
-									</div> */}
+									</div> 
                   <div className="c-info">
                     {/* <form action="" className='c-info__form'>                 */}
                     <h2 className="c-info__subheader">
